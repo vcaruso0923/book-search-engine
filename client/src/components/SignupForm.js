2d20c3742479d68f5b8d8e6ac77ba6c3b635f8d8
vcaruso <vinnycar0923@gmail.com>
2020-10-13 16:35:00 -0700

    fixed sign up

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,20 +32,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser({
-        variables: { userFormData }
+      const {data} = await createUser({
+        variables: { ...userFormData }
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.createUser.token);
+    } catch (e) {
+      console.error(e);
     }
 
     setUserFormData({
